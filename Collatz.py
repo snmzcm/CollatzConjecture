a2a160af86071ac5a7c70d2fe6f0e901d3255d0d
Misand <misnadqasim@gmail.com>
2021-08-08 06:56:58 +0300

    improved code

--- Collatz.py
+++ Collatz.py
@@ -1,49 +1,40 @@
-#Author Ali Cem Sonmez(snmzcm)
+
+__author__ = "Ali Cem Sonmez(snmzcm)"
+__license__ = "MIT License"
+__version__ = "1.1"
+
+# Contributers:-
+# [Misnad](https://github.com/Misnad)
+
+
+
 def main():
-    def ParityCheck(numberX):
-            numberX = int(numberX)
-            if numberX % 2 == 0:
-                return True
-            else:
+    def ParityCheck(numberX: int):
+            if numberX % 2:
                 return False
+            return True
+
+
     def Collatz(x):
-            parity = ParityCheck(x)
-            if parity == True:
-                print(x," -Even Number")
+            if ParityCheck(x):
+                print(x,"\t-Even Number")
                 x = x // 2
                 if x == 1:
-                    print(1, " -Odd Number")
+                    print(1, "\t-Odd Number\n\n")
                     return 1
-                else:
-                    Collatz(x)
-                    
-            elif parity == False:
-                print(x, " -Odd Number")
-                x = 3 * x + 1;
-                if x == 1:
-                    print(1, " -Odd Number")
-                    return 1
-                else:
-                    Collatz(x)
-    
-    
-    
+                Collatz(x)
+            else:
+                print(x, "\t-Odd Number")
+                Collatz(3 * x + 1)
+
+
     while True:
-        numb = int(input("Enter a positive integer\n"))
+        numb = int(input("Enter a positive integer: "))
         if numb > 0:
             Collatz(numb)
         else:
            print("Number has to be a positive integer, Try again!")
-    
-    
 
 
-
-
-
-
-
-
-
-
-main()
+if __name__=='__main__':
+    main()
